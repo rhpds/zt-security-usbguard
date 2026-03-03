b270dd0c3174d76a8f6ec44ab4e0f78fd4a3e890
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2026-03-03 03:07:50 -0500

    Update setup-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -13,3 +13,10 @@ chmod 666 /tmp/progress.log
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
 
 dnf install -y certbot
+
+# Enable cockpit functionality in showroom.
+dnf -y remove tlog cockpit-session-recording
+echo "[WebService]" > /etc/cockpit/cockpit.conf
+echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
+echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
+systemctl enable --now cockpit.socket
